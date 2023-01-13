7f959f56feedab4dce9e1d048ead9243f304dcc5
codytseng <codytseng@outlook.com>
2023-01-13 10:31:42 +0800

    test: update runner unit testing

--- __test__/run.spec.ts
+++ __test__/run.spec.ts
@@ -3,8 +3,10 @@ import * as github from '@actions/github';
 import { describe, expect, jest, test } from '@jest/globals';
 import { CommentService } from '../src/comment';
 import { MergeableState } from '../src/enum';
+import { LabelService } from '../src/label';
 import { PullRequestService } from '../src/pull-request';
 import { Runner } from '../src/run';
+import { QueryService } from '../src/query';
 
 describe('run', () => {
   test('should succeed', async () => {
@@ -13,6 +15,7 @@ describe('run', () => {
       'wait-ms': '3000',
       'max-retries': '5',
       'comment-body': 'Merge Conflict',
+      'label-name': 'conflict',
     };
 
     (github as any).context = {
@@ -45,12 +48,32 @@ describe('run', () => {
           labels: { nodes: [] },
         },
       ]);
+    jest
+      .spyOn(QueryService.prototype, 'getRepositoryLabels')
+      .mockResolvedValue({
+        repository: {
+          labels: {
+            nodes: [
+              {
+                id: 'labelId',
+                name: input['label-name'],
+              },
+            ],
+          },
+        },
+      });
 
     const mockAddMergeConflictCommentIfNeed = jest
       .spyOn(CommentService.prototype, 'addMergeConflictCommentIfNeed')
       .mockResolvedValue(true);
     const mockDeleteMergeConflictCommentIfNeed = jest
       .spyOn(CommentService.prototype, 'deleteMergeConflictCommentIfNeed')
+      .mockResolvedValue(false);
+    const mockAddMergeConflictLabelIfNeed = jest
+      .spyOn(LabelService.prototype, 'addMergeConflictLabelIfNeed')
+      .mockResolvedValue(true);
+    const mockRemoveMergeConflictLabelIfNeed = jest
+      .spyOn(LabelService.prototype, 'removeMergeConflictLabelIfNeed')
       .mockResolvedValue(false);
 
     const runner = new Runner();
@@ -59,5 +82,7 @@ describe('run', () => {
 
     expect(mockAddMergeConflictCommentIfNeed).toBeCalledTimes(1);
     expect(mockDeleteMergeConflictCommentIfNeed).toBeCalledTimes(1);
+    expect(mockAddMergeConflictLabelIfNeed).toBeCalledTimes(1);
+    expect(mockRemoveMergeConflictLabelIfNeed).toBeCalledTimes(1);
   });
 });
